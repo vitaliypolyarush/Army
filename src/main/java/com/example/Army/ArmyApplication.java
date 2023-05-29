1a420d73015e490d2ef4cba6d9ecdd711ab0f053
Maksopinto <maksopinto@gmail.com>
2023-05-29 20:22:14 +0300

    Initial commit

---
+++ src/main/java/com/example/Army/ArmyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Army;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Army/ArmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Army;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
